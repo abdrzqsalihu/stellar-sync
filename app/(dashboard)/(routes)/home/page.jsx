7f571eb583d9ebaa3954a791c72474a3939e4cfb
Abdulrazaq Salihu <abdrzq.salihu@gmail.com>
2024-03-23 22:24:03 +0100

    integrated the overview component

--- app/(dashboard)/(routes)/home/page.jsx
+++ app/(dashboard)/(routes)/home/page.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Overview from "./_components/Overview";
 
 function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <div className="mx-auto px-10 mt-10">
+      <Overview />
+    </div>
+  );
 }
 
 export default HomePage;
