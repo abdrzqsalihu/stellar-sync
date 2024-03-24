23da0420cca05abd613852b7428d631f02bf8a5f
Abdulrazaq Salihu <abdrzq.salihu@gmail.com>
2024-03-24 12:02:34 +0100

    implemented the uploadfile component

--- app/(dashboard)/(routes)/home/page.jsx
+++ app/(dashboard)/(routes)/home/page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Overview from "./_components/Overview";
+import UploadFile from "./_components/UploadFile";
 
 function HomePage() {
   return (
     <div className="mx-auto px-10 mt-10">
       <Overview />
+      <UploadFile />
     </div>
   );
 }
