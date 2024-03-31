4a8b5416917f83f23e77ec190cf2121c325b1862
Abdulrazaq Salihu <abdrzq.salihu@gmail.com>
2024-03-31 17:43:21 +0100

    integrated vercel speed insights

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Rubik } from "next/font/google";
 import "./globals.css";
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
         <body className={rubik.className}>
           {children}
           <Analytics />
+          <SpeedInsights />
         </body>
       </html>
     </ClerkProvider>
